1de029e80d3a6bd73f009db563774a3f4b249e49
Christian Metge <49899763+christmichee123@users.noreply.github.com>
2021-11-08 15:00:58 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,33 @@ include "./letextoservice.php";
 //On recupere les informations pour l'envoi
 //Par exemple le nom et les contacts
 $letextoService = new LeTExtoService();
+//Liste d'envoi
 $contact = [
-    ['phone'=>'225XXXXXXX'],
-    ['phone'=>'225XXXXXXXX'],
-    ];
+            [
+              'phone' => '22508080808',
+            ],
+     [
+              'phone' => '225023009876',
+            ],
+     [
+              'phone' => '22506080808',
+            ]
+          ];
+//Parametre du texto
+$param= [
+          'step' => NULL,
+          'sender' => 'SIGES-DCF',
+          'name' => 'Notification nvle agent',
+          'campaignType' => 'SIMPLE',
+          'recipientSource' => 'CUSTOM',
+          'groupId' => NULL,
+          'filename' => NULL,
+          'saveAsModel' => false,
+          'destination' => 'NAT_INTER',
+          'message' => 'message',
+          'emailText' => NULL,
+          'recipients' => $contact,
+          'sendAt' => [],
+        ];
 $idCampaign = $letextoService->setCampaign($param);
 $sendCampaign = $letextoServie->sendSms($idCampaign);
